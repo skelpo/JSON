0e920de558ec0972752f01b27cab25faff331c68
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-02 10:08:48 -0600

    Created JSON.CodingKeys sub-type

--- Sources/JSON/JSON.swift
+++ Sources/JSON/JSON.swift
@@ -1,8 +1,22 @@
-public enum JSON {
+public enum JSON: Codable {
     case null
     case string(String)
     case number(Number)
     case bool(Bool)
     indirect case array([JSON])
     indirect case object([String: JSON])
+    
+    public struct CodingKeys: CodingKey {
+        public var stringValue: String
+        public var intValue: Int?
+        
+        public init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        
+        public init?(intValue: Int) {
+            self.init(stringValue: "\(intValue)")
+            self.intValue = intValue
+        }
+    }
 }
