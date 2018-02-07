432bad5b96dc3507d4bf5f74d7b455962647b291
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-07 09:14:00 -0600

    Created default implementation for FailableJSONRepresentable protocol

--- Sources/JSONKit/JSONConvertible.swift
+++ Sources/JSONKit/JSONConvertible.swift
@@ -1,11 +1,27 @@
 import Foundation
 
+// MARK: - Protocols
+
 public protocol JSONRepresentable {
     var json: JSON { get }
 }
 
 public protocol FailableJSONRepresentable {
     func json()throws -> JSON
+}
+
+// MARK: - Default Converters
+
+extension FailableJSONRepresentable where Self: JSONRepresentable {
+    public func json()throws -> JSON {
+        return self.json
+    }
+}
+
+extension FailableJSONRepresentable where Self: Encodable {
+    public func json()throws -> JSON {
+        return try (self as Encodable).json()
+    }
 }
 
 extension Decodable {
