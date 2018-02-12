cde0dabc0e9a22301e2fc7b06dc7628a23d957fd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-12 11:03:54 -0600

    Created JSON.response method

--- Sources/JSONKit/JSON+HTTP.swift
+++ Sources/JSONKit/JSON+HTTP.swift
@@ -6,3 +6,11 @@ extension JSON: HTTPBodyRepresentable {
         return try HTTPBody(JSONEncoder().encode(self))
     }
 }
+
+extension JSON {
+    func response(withStatus status: HTTPStatus = .ok, headers: HTTPHeaders = [:])throws -> HTTPResponse {
+        var header = headers
+        header[.contentType] = "application/json"
+        return try HTTPResponse(status: status, headers: header, body: self)
+    }
+}
