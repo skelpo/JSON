b97d1adbc15f788d6ac6fbecbd6b3a8bb1011dd6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-13 14:59:14 -0600

    Tagged Vapor dependency at 3.0.0-beta.2

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "JSONKit", targets: ["JSONKit", "JSON"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .branch("beta"))
+        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-beta.2"))
     ],
     targets: [
         .target(name: "JSON", dependencies: []),
