73d048571a45c8d737d2da71246b2cc680d329a3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-15 16:11:02 -0600

    Created JSON.set method for FailableJSONRepresentable types

--- Sources/JSON/JSON.swift
+++ Sources/JSON/JSON.swift
@@ -64,6 +64,10 @@ public enum JSON: Codable {
         }
     }
     
+    public mutating func set(_ key: String, to json: FailableJSONRepresentable)throws {
+        try self.set(key, json.failableJSON())
+    }
+    
     public func merge(_ json: JSON)throws -> JSON {
         guard case let JSON.object(new) = json, case var JSON.object(this) = self else {
             throw JSONError.unableToMergeCases(self, json)
