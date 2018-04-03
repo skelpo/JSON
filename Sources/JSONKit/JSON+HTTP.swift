0cc37aa34d9eb3fceda5e61bfbb66b6541319ea8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-03 15:18:53 -0500

    Updated JSONKit for Vapor RC2 with Swift NIO

--- Sources/JSONKit/JSON+HTTP.swift
+++ Sources/JSONKit/JSON+HTTP.swift
@@ -1,16 +1,16 @@
-import HTTP
+import Vapor
 import Foundation
 
 extension JSON: HTTPBodyRepresentable {
     public func makeBody() throws -> HTTPBody {
-        return try HTTPBody(JSONEncoder().encode(self))
+        return try HTTPBody(data: JSONEncoder().encode(self))
     }
 }
 
 extension JSON {
     func response(withStatus status: HTTPStatus = .ok, headers: HTTPHeaders = [:])throws -> HTTPResponse {
         var header = headers
-        header[.contentType] = "application/json"
-        return try HTTPResponse(status: status, headers: header, body: self)
+        header.replaceOrAdd(name: .contentType, value: "application/json")
+        return try HTTPResponse(status: status, headers: header, body: self.makeBody())
     }
 }
