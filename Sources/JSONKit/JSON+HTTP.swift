845ae81c1d50ace5f915f233e3d12cd4a8d855b9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-23 15:46:05 -0500

    Removed JSON.HTTPBodyRepresentable conformance and created JSON.body() method

--- Sources/JSONKit/JSON+HTTP.swift
+++ Sources/JSONKit/JSON+HTTP.swift
@@ -1,16 +1,14 @@
 import Vapor
 import Foundation
 
-extension JSON: HTTPBodyRepresentable {
-    public func makeBody() throws -> HTTPBody {
+extension JSON {
+    func body()throws -> HTTPBody {
         return try HTTPBody(data: JSONEncoder().encode(self))
     }
-}
-
-extension JSON {
+    
     func response(withStatus status: HTTPStatus = .ok, headers: HTTPHeaders = [:])throws -> HTTPResponse {
         var header = headers
         header.replaceOrAdd(name: .contentType, value: "application/json")
-        return try HTTPResponse(status: status, headers: header, body: self.makeBody())
+        return try HTTPResponse(status: status, headers: header, body: self.body())
     }
 }
