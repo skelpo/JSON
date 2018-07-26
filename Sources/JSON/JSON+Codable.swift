dbb2dbd2f1ad9bfc1aa6b8780c557a9d74aecadd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-26 08:47:11 -0500

    Use inout paramaters for JSON codable

--- Sources/JSON/JSON+Codable.swift
+++ Sources/JSON/JSON+Codable.swift
@@ -1,9 +1,9 @@
 extension JSON {
     public init(from decoder: Decoder) throws {
-        if let container = try? decoder.container(keyedBy: JSON.CodingKeys.self) {
-            self = try JSON(from: container)
-        } else if let container = try? decoder.unkeyedContainer() {
-            self = try JSON(from: container)
+        if var container = try? decoder.container(keyedBy: JSON.CodingKeys.self) {
+            self = try JSON(from: &container)
+        } else if var container = try? decoder.unkeyedContainer() {
+            self = try JSON(from: &container)
         } else {
             let container = try decoder.singleValueContainer()
             self = try JSON(from: container)
@@ -13,14 +13,14 @@ extension JSON {
     public func encode(to encoder: Encoder) throws {
         switch self {
         case let .object(structure):
-            let container = encoder.container(keyedBy: JSON.CodingKeys.self)
-            try self.encode(object: structure, with: container)
+            var container = encoder.container(keyedBy: JSON.CodingKeys.self)
+            try self.encode(object: structure, with: &container)
         case let .array(sequence):
-            let container = encoder.unkeyedContainer()
-            try self.encode(array: sequence, with: container)
+            var container = encoder.unkeyedContainer()
+            try self.encode(array: sequence, with: &container)
         default:
-            let container = encoder.singleValueContainer()
-            try self.encode(value: self, with: container)
+            var container = encoder.singleValueContainer()
+            try self.encode(value: self, with: &container)
         }
     }
     
@@ -69,10 +69,8 @@ extension JSON {
         return .array(elements)
     }
     
-    private init(from container: KeyedDecodingContainer<JSON.CodingKeys>)throws {
-        var object: [String: JSON] = [:]
-        
-        try container.allKeys.forEach { (key) in
+    private init(from container: inout KeyedDecodingContainer<JSON.CodingKeys>)throws {
+        let object: [String: JSON] = try container.allKeys.reduce(into: [:]) { object, key in
             if let value = try? container.decodeNil(forKey: key), value {
                 object[key.stringValue] = .null
             } else if let value = try? container.decode(String.self, forKey: key) {
@@ -81,22 +79,22 @@ extension JSON {
                 object[key.stringValue] = .number(value)
             } else if let value = try? container.decode(Bool.self, forKey: key) {
                 object[key.stringValue] = .bool(value)
-            } else if let con = try? container.nestedContainer(keyedBy: JSON.CodingKeys.self, forKey: key) {
-                object[key.stringValue] = try JSON(from: con)
-            } else if let con = try? container.nestedUnkeyedContainer(forKey: key) {
-                object[key.stringValue] = try JSON(from: con)
+            } else if var con = try? container.nestedContainer(keyedBy: JSON.CodingKeys.self, forKey: key) {
+                object[key.stringValue] = try JSON(from: &con)
+            } else if var con = try? container.nestedUnkeyedContainer(forKey: key) {
+                object[key.stringValue] = try JSON(from: &con)
             } else {
                 throw DecodingError.dataCorruptedError(forKey: key, in: container, debugDescription: "Un-supported type found in JSON")
             }
         }
-        
+
         self = .object(object)
     }
     
-    private init(from c: UnkeyedDecodingContainer)throws {
-        var container = c
+    private init(from container: inout UnkeyedDecodingContainer)throws {
         var array: [JSON] = []
         
+        array.reserveCapacity(container.count ?? 0)
         while !container.isAtEnd {
             if let value = try? container.decodeNil(), value {
                 array.append(.null)
@@ -106,10 +104,10 @@ extension JSON {
                 array.append(.number(value))
             } else if let value = try? container.decode(Bool.self) {
                 array.append(.bool(value))
-            } else if let con = try? container.nestedContainer(keyedBy: JSON.CodingKeys.self) {
-                try array.append(JSON(from: con))
-            } else if let con = try? container.nestedUnkeyedContainer() {
-                try array.append(JSON(from: con))
+            } else if var con = try? container.nestedContainer(keyedBy: JSON.CodingKeys.self) {
+                try array.append(JSON(from: &con))
+            } else if var con = try? container.nestedUnkeyedContainer() {
+                try array.append(JSON(from: &con))
             } else {
                 throw DecodingError.dataCorruptedError(in: container, debugDescription: "Un-supported type found in JSON")
             }
@@ -132,10 +130,8 @@ extension JSON {
         }
     }
     
-    private func encode(object: [String: JSON], with c: KeyedEncodingContainer<JSON.CodingKeys>) throws {
-        var container = c
-        
-        try object.forEach({ (key, value) in
+    private func encode(object: [String: JSON], with container: inout KeyedEncodingContainer<JSON.CodingKeys>) throws {
+        try object.forEach { key, value in
             let key = CodingKeys(stringValue: key)
             
             switch value {
@@ -144,37 +140,33 @@ extension JSON {
             case let .number(value): try container.encode(value, forKey: key)
             case let .bool(value): try container.encode(value, forKey: key)
             case let .object(value):
-                let con = container.nestedContainer(keyedBy: JSON.CodingKeys.self, forKey: key)
-                try encode(object: value, with: con)
+                var con = container.nestedContainer(keyedBy: JSON.CodingKeys.self, forKey: key)
+                try encode(object: value, with: &con)
             case let .array(value):
-                let con = container.nestedUnkeyedContainer(forKey: key)
-                try encode(array: value, with: con)
+                var con = container.nestedUnkeyedContainer(forKey: key)
+                try encode(array: value, with: &con)
             }
-        })
+        }
     }
     
-    private func encode(array: [JSON], with c: UnkeyedEncodingContainer) throws {
-        var container = c
-        
-        try array.forEach { (element) in
+    private func encode(array: [JSON], with container: inout UnkeyedEncodingContainer) throws {
+        try array.forEach { element in
             switch element {
             case .null: try container.encodeNil()
             case let .string(value): try container.encode(value)
             case let .number(value): try container.encode(value)
             case let .bool(value): try container.encode(value)
             case let .object(value):
-                let con = container.nestedContainer(keyedBy: JSON.CodingKeys.self)
-                try encode(object: value, with: con)
+                var con = container.nestedContainer(keyedBy: JSON.CodingKeys.self)
+                try encode(object: value, with: &con)
             case let .array(value):
-                let con = container.nestedUnkeyedContainer()
-                try encode(array: value, with: con)
+                var con = container.nestedUnkeyedContainer()
+                try encode(array: value, with: &con)
             }
         }
     }
     
-    private func encode(value: JSON, with c: SingleValueEncodingContainer)throws {
-        var container = c
-        
+    private func encode(value: JSON, with container: inout SingleValueEncodingContainer)throws {
         switch value {
         case .null: try container.encodeNil()
         case let .string(value): try container.encode(value)
