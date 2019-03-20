d8896efb42df843ce1b2f8bf065583dc703550ea
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-20 16:44:10 -0500

    Rewote JSON enum helper methods/properties around updates in Swift 5

--- Sources/JSON/JSON.swift
+++ Sources/JSON/JSON.swift
@@ -1,111 +1,331 @@
-#if swift(>=4.2)
 @dynamicMemberLookup
-#endif
-public enum JSON: Codable, Equatable, CustomStringConvertible {
+public enum JSON: Equatable, CustomStringConvertible {
+    
+    // MARK: - Properties
+    
+    /// Gets the value of a `.null` case.
+    ///
+    /// You will always get `nil` from this property. This is because the
+    /// JSON case is itself `.null`, or it isn't so conversion failed and you get `nil`.
+    ///
+    /// The `Wrapped` type is `Never` so the compiler can gurantee that the value is `nil`.
+    public var null: Optional<Never> {
+        get { return nil }
+        set { self = .null }
+    }
+    
+    /// Accesses the value of a `.bool` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var bool: Bool? {
+        get {
+            guard case let .bool(value) = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(JSON.bool) ?? .null
+        }
+    }
+    
+    /// Accesses the value of a `.string` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var string: String? {
+        get {
+            guard case let .string(value) = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(JSON.string) ?? .null
+        }
+    }
+    
+    /// Accesses the value of a `Number.int` case wrapped in a `.number` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var int: Int? {
+        get {
+            guard case let .number(.int(value)) = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(Number.int).map(JSON.number) ?? .null
+        }
+    }
+    
+    /// Accesses the value of a `Number.float` case wrapped in a `.number` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var float: Float? {
+        get {
+            guard case let .number(.float(value))  = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(Number.float).map(JSON.number) ?? .null
+        }
+    }
+    
+    /// Accesses the value of a `Number.double` case wrapped in a `.number` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var double: Double? {
+        get {
+            guard case let .number(.double(value)) = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(Number.double).map(JSON.number) ?? .null
+        }
+    }
+    
+    /// Accesses the value of an `.array` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var array: [JSON]? {
+        get {
+            guard case let .array(value) = self else { return nil }
+            return value
+        }
+        set {
+            self = newValue.map(JSON.array) ?? .null
+        }
+    }
+    
+    /// Accesses the value of an `.object` case.
+    ///
+    /// The setter will set the JSON case regardles the current type.
+    public var object: [String: JSON]? {
+        get {
+            guard case let .object(value) = self else { return nil }
+            return value
+        }
+        set {
+             self = newValue.map(JSON.object) ?? .null
+        }
+    }
+    
+    /// Converts a non-optiona JSON case to an optional JSON case.
+    ///
+    /// If the JSON case is `.null`, then `nil` will be returned. Otherwise the JSON case is returned.
+    /// This allows you to unwrap a case to make sure it isn't `null`:
+    ///
+    ///     if let value = json.optional {
+    ///         // ...
+    ///     }
+    public var optional: Optional<JSON> {
+        switch self {
+        case .null: return nil
+        default: return self
+        }
+    }
     
     // MARK: - Cases
+    
+    /// A `null` JSON value.
+    ///
+    /// This represents an explicit `null` value in the JSON. A non-existant key or index has no representation.
+    ///
+    ///     {
+    ///         "foo": null
+    ///     }
     case null
-    case string(String)
-    case number(Number)
+    
+    /// A boolean JSON value.
+    ///
+    ///     {
+    ///         "fizz": true,
+    ///         "buzz": false
+    ///     }
     case bool(Bool)
-    indirect case array([JSON])
-    indirect case object([String: JSON])
-
+    
+    /// A string JSON value.
+    ///
+    ///     {
+    ///         "foo": "bar"
+    ///     }
+    case string(String)
+    
+    /// A number JSON value.
+    ///
+    /// This case wraps a `Number` enum case, which will be an int, float, or double.
+    ///
+    ///     {
+    ///         "answer": 42,
+    ///         "pi": 3.1415
+    ///     }
+    case number(Number)
+    
+    /// A JSON array of values.
+    ///
+    ///     [
+    ///         "foo",
+    ///         "bar",
+    ///         1997,
+    ///     ]
+    case array([JSON])
+    
+    /// A JSON object which maps string keys to JSON values.
+    ///
+    ///     {
+    ///         "foo: "bar",
+    ///         "fizz": true,
+    ///         "bar": [98, 97, 114]
+    ///     }
+    case object([String: JSON])
+    
+    
+    // MARK: - Initializers
+    
+    /// Creates a `JSON` instance with the `.null` case.
+    public init() {
+        self = .null
+    }
+    
+    /// Creates a `JSON` instance with the `.string` case.
+    ///
+    /// - Parameter string: The `String` held by the `.string` case.
+    public init(_ string: String) {
+        self = .string(string)
+    }
+    
+    /// Creates a `JSON` instance with the `.bool` case.
+    ///
+    /// - Parameter bool: The `Bool` held by the `.bool` case.
+    public init(_ bool: Bool) {
+        self = .bool(bool)
+    }
+    
+    /// Creates a `JSON` instance with the `.number(Number.int)` case.
+    ///
+    /// - Parameter int: The `Int` held by the `.int` case.
+    public init(_ int: Int) {
+        self = .number(.int(int))
+    }
+    
+    /// Creates a `JSON` instance with the `.number(Number.float)` case.
+    ///
+    /// - Parameter float: The `Float` held by the `.float` case.
+    public init(_ float: Float) {
+        self = .number(.float(float))
+    }
+    
+    /// Creates a `JSON` instance with the `.number(Number.double)` case.
+    ///
+    /// - Parameter double: The `Double` held by the `.double` case.
+    public init(_ double: Double) {
+        self = .number(.double(double))
+    }
+    
+    /// Creates a `JSON` instance with the `.array` case.
+    ///
+    /// - Parameter array: The `JSON` values held by the `.array` case.
+    public init(_ array: [JSON]) {
+        self = .array(array)
+    }
+    
+    /// Creates a `JSON` instance with the `.object` case.
+    ///
+    /// - Parameter array: The `JSON` key/values map held by the `.object` case.
+    public init(_ object: [String: JSON]) {
+        self = .object(object)
+    }
+    
     // MARK: - Public Methods
-    #if swift(>=4.2)
+    
+    /// Gets the JSON value for a given key.
+    ///
+    /// Most of the time, the member passed in with be a `String` key. This will get a value for an object key:
+    ///
+    ///     json.user.first_name // .string("Tanner")
+    ///
+    /// However, `Int` members are also supported in that case of a `JSON` array:
+    ///
+    ///     json.users.0.age // .number(.int(42))
+    ///
+    /// - Parameter member: The key for JSON objects or index for JSON arrays of the value to get.
+    ///
+    /// - Returns: The JSON value for the given key or index. `.null` is returned if the value is not found or
+    ///   the JSON value this subscript is called on does not have nested data, i.e. `.string`, `.bool`, etc.
     public subscript(dynamicMember member: String) -> JSON {
-        switch self {
-        case let .object(structure): return structure[member] ?? .null
-        default: return .null
-        }
-    }
-    #endif
-    
-    public subscript(_ key: String...) -> JSON? {
-        do {
-            return try self.element(at: key)
-        } catch {
-            return nil
-        }
-    }
-    
-    public subscript(_ index: Int) -> JSON? {
-        switch self {
-        case let .array(sequence):
-            guard index < self.count! && index >= 0 else {
-                return nil
-            }
-            return sequence[index]
-        default: return nil
-        }
-    }
-    
-    public mutating func insert(_ json: JSON, at index: Int)throws {
-        switch self {
-        case var .array(sequence):
-            sequence.insert(json, at: index)
-            self = .array(sequence)
-        default: throw JSONError.unableToMergeCases(self, json)
-        }
-    }
-    
-    public mutating func append(_ json: JSON)throws {
-        switch self {
-        case var .array(sequence):
-            sequence.append(json)
-            self = .array(sequence)
-        default: throw JSONError.unableToMergeCases(self, json)
-        }
-    }
-    
-    public mutating func append(_ json: FailableJSONRepresentable)throws {
-        try self.append(json.failableJSON())
-    }
-    
-    public mutating func set(_ key: String, _ json: JSON)throws {
-        switch self {
-        case var .object(structure):
-            structure[key] = json
-            self = .object(structure)
-        default: throw JSONError.unableToMergeCases(self, json)
-        }
-    }
-    
-    public mutating func set(_ key: String, to json: FailableJSONRepresentable)throws {
-        try self.set(key, json.failableJSON())
-    }
-    
-    public func merge(_ json: JSON)throws -> JSON {
-        guard case let JSON.object(new) = json, case var JSON.object(this) = self else {
-            throw JSONError.unableToMergeCases(self, json)
-        }
-        this.merge(new) { (_, new) -> JSON in new }
-        return .object(this)
-    }
-    
-    public func element(at path: [String])throws -> JSON {
-        var current: JSON = self
-        
-        try path.forEach { key in
-            switch current {
-            case .object: current = try current.getSingleLevelElement(with: key)
-            case .array: current = try current.getElementsFromArray(with: key)
-            default: throw JSONError.badPathAtKey(path, key)
+        get {
+            switch self {
+            case let .object(structure): return structure[member] ?? .null
+            case let .array(sequence) where Int(member) != nil:
+                if let index = Int(member) {
+                    guard index >= 0 && index < sequence.endIndex else { return .null }
+                    return sequence[index]
+                } else {
+                    let elements = sequence.map { element in element[dynamicMember: member] }
+                    return .array(elements)
+                }
+            default: return .null
             }
         }
-        
-        return current
-    }
-    
-    public var count: Int? {
-        switch self {
-        case let .string(string): return string.count
-        case let .array(sequence): return sequence.count
-        case let .object(structure): return structure.count
-        default: return nil
+        set {
+            switch self {
+            case var .object(structure):
+                structure[member] = newValue
+                self = .object(structure)
+            case var .array(sequence) where Int(member) != nil:
+                guard let index = Int(member) else { return }
+                sequence[index] = newValue
+                self = .array(sequence)
+            default: self = newValue
+            }
         }
     }
     
+    /// Gets the JSON at a given path.
+    ///
+    /// Depending on the JSON case for the path element, different logic paths will be taken:
+    /// - `.object`: Get the JSON value where the key is equal to the path element.
+    /// - `.array`:
+    ///   - `Int`: Get the array element at the given index.
+    ///   - `String`: Get the value for the key from each array element, and return them as an array.
+    /// - default: Return `.null`.
+    ///
+    /// - Parameter path: The keys and indexes to the desired JSON value(s).
+    /// - Returns: Thw JSON value(s) found at the path passed in. You will get a `.null` case
+    ///   if no JSON is found at the given path.
+    public func get(_ path: [String]) -> JSON {
+        return path.reduce(self) { json, key in
+            switch json {
+            case let .object(object): return object[key] ?? .null
+            case let .array(array):
+                if let index = Int(key) {
+                    return array[index]
+                } else {
+                    let new = array.map { element in
+                        return element.get([key])
+                    }
+                    return .array(new)
+                }
+            default: return .null
+            }
+        }
+    }
+    
+    /// Sets the value of an object key or array index.
+    ///
+    ///
+    ///
+    /// - Parameters:
+    ///   - path: The path of the value to set.
+    ///   - json: The JSON value to set the index or key to.
+    public mutating func set<Path>(_ path: Path, to json: JSON) where Path: Collection, Path.Element == String {
+        if let key = path.first {
+            var current = self.get([key])
+            current.set(path.dropFirst(), to: json)
+            self = current
+        } else {
+            self = json
+        }
+    }
+    
+    /// See `CustomStringConvertible.description`.
+    ///
+    /// This textal representation is compressed, so you might need to prettify it to read it.
     public var description: String {
         switch self {
         case .null: return "null"
